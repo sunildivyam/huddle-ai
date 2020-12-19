6b82b9eb1a7335486f15f3fa009501cac221ea6b
Sunil Kumar <sunil.divyam@gmail.com>
2020-12-19 20:21:08 +0530

    Implemented Post Overview Page with comments

--- src/components/post-page/PostPage.tsx
+++ src/components/post-page/PostPage.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
-import { ArrowLeftCircleFill, PersonFill } from 'react-bootstrap-icons';
+import { ArrowLeftCircleFill } from 'react-bootstrap-icons';
 import { Link, useParams } from 'react-router-dom';
 import { IPost } from '../../models';
 import { getPost } from '../../services';
-import { Posts } from '../posts/Posts';
+import { Post } from '../../components';
 import './PostPage.scss';
 
 interface IParams {
@@ -21,42 +21,13 @@ export function PostPage() {
         .catch((post: IPost) => setPost(post))
     }, []);
 
-    function renderComments() {
-        return post.comments && post.comments.map(comment => (
-        <Card key={comment.id} className="comment">
-            <Card.Header>
-                <Card.Title>{comment.name}</Card.Title>
-                <Card.Subtitle>
-                    <PersonFill/>{comment.email}
-                </Card.Subtitle>
-            </Card.Header>
-            <Card.Body>
-                <Card.Text>{comment.body}</Card.Text>
-            </Card.Body>
-        </Card>
-        )) || [];
-    }
     return (
         <Card className="post-page">
             <Card.Header>
             <Link to={`/posts`}><ArrowLeftCircleFill className="back" size="40"/>back to Posts</Link>
             </Card.Header>
             <Card.Body>
-                <Card.Title>{post.title}</Card.Title>
-                <Card.Subtitle>
-                {post.user && <Link to={`/users/${post.user.id}`}>
-                    <PersonFill className="post-user"/>{post.user.username}
-                </Link>}
-                </Card.Subtitle>
-                <Card.Text>{post.body}</Card.Text>
-                <Card className="comments">
-                    <Card.Body>
-                        <Card.Title>Comments:</Card.Title>
-                        {
-                            renderComments()
-                        }
-                    </Card.Body>
-                </Card>
+                <Post post={post}></Post>
             </Card.Body>
         </Card>
     );
--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { PersonFill } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
+import { IPost } from '../../models';
 
-export function Post() {
+interface IProps {
+    post: IPost;
+}
+export function Post({ post }: IProps) {
 
-    return (
-        <Card>
+    function renderComments() {
+        return post.comments && post.comments.map(comment => (
+        <Card key={comment.id} className="comment">
+            <Card.Header>
+                <Card.Title>{comment.name}</Card.Title>
+                <Card.Subtitle>
+                    <PersonFill/>{comment.email}
+                </Card.Subtitle>
+            </Card.Header>
             <Card.Body>
-                Post View
+                <Card.Text>{comment.body}</Card.Text>
             </Card.Body>
         </Card>
+        )) || [];
+    }
+
+    return (
+        <>
+            <Card.Title>{post.title}</Card.Title>
+            <Card.Subtitle>
+                {post.user && <Link to={`/users/${post.user.id}`}>
+                    <PersonFill className="post-user" />{post.user.username}
+                </Link>}
+            </Card.Subtitle>
+            <Card.Text>{post.body}</Card.Text>
+            <Card className="comments">
+                <Card.Body>
+                    <Card.Title>Comments:</Card.Title>
+                    {
+                        renderComments()
+                    }
+                </Card.Body>
+            </Card>
+        </>
     );
 }
